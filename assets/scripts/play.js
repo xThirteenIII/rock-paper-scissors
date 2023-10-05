12e32c7577edad47a376265528d24925acaee0eb
Jisaa <giuseppe.richetti@gmail.com>
2023-10-05 17:53:47 +0200

    Remove five round logic

--- assets/scripts/play.js
+++ assets/scripts/play.js
@@ -1,3 +1,11 @@
+
+//  For now, remove the logic that plays exactly five rounds.
+//  Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+//  Add a div for displaying results and change all of your console.logs into DOM methods.
+//  Display the running score, and announce a winner of the game once one player reaches 5 points.
+//  You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
+
+
 "use strict"
 
 //Global variables
@@ -80,15 +88,9 @@ function playRound(){
 
 function bestOfFive(){
 
-    while (playerScore != 3 && computerScore != 3) {
+    while ( 0==0){
         playRound();
     };
-
-    if (playerScore == 3){
-        console.log("You won! :D")
-    }else if (computerScore == 3){
-        console.log("CPU won! D:")
-    }
 
     return;
 }
